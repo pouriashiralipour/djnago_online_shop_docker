21397e0c849f3a3a7351d5290f4b6b42344e6e1b
pouriashiralipour <pouriashiralipour@gmail.com>
2023-03-08 19:44:04 +0330

    delete view and urls from accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
-from .views import SignUpView
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='sign_up'),
-]
+# from django.urls import path
+#
+# from .views import SignUpView
+#
+# urlpatterns = [
+#     path('signup/', SignUpView.as_view(), name='sign_up'),
+# ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
-from .forms import CustomUserCreationForm
-
-
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = 'registration/sign_up.html'
-    success_url = reverse_lazy('login')
+# from django.urls import reverse_lazy
+# from django.views.generic import CreateView
+#
+# from .forms import CustomUserCreationForm
+#
+#
+# class SignUpView(CreateView):
+#     form_class = CustomUserCreationForm
+#     template_name = 'registration/sign_up.html'
+#     success_url = reverse_lazy('login')
